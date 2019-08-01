adc495a70b24119611f44bed38651a497021e85b
Preeti <singhp1992@gmail.com>
2019-08-01 17:31:48 +0200

    cleaning up code

--- src/App.vue
+++ src/App.vue
@@ -36,16 +36,10 @@ export default class App extends Vue {}
   color: black;
 }
 
-/* #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
+
+ #nav {
   padding: 30px;
-}
+} 
 
 #nav a {
   font-weight: bold;
@@ -54,5 +48,5 @@ export default class App extends Vue {}
 
 #nav a.router-link-exact-active {
   color: #42b983;
-} */
+} 
 </style>
